16222fc6aaeef18a40123fe0a8e6a4d6e5cc0aea
Florian Beaufumé <fbeaufume@gmail.com>
2023-01-28 11:36:05 +0100

    Added attack and retaliation

--- src/main/kotlin/com/adeliosys/heroes3simulator/CreatureStack.kt
+++ src/main/kotlin/com/adeliosys/heroes3simulator/CreatureStack.kt
@@ -1,6 +1,5 @@
 package com.adeliosys.heroes3simulator
 
-import kotlin.math.max
 import kotlin.math.min
 
 /**
@@ -10,12 +9,33 @@ class CreatureStack(val creature: Creature, val initialQuantity: Int) {
 
     var quantity = initialQuantity
 
+    fun attack(other: CreatureStack) {
+        // This creature attacks the other one
+        var damage = computeDamage(other.creature)
+        other.applyDamage(damage)
+
+        println("${creature.name} attacks ${other.creature.name} for $damage damage: ${other.quantity} left (${other.creature.currentHealth} health)")
+
+        // The other one retaliates, if possible
+        if (other.isAlive()) {
+            damage = other.computeDamage(this.creature)
+            applyDamage(damage)
+
+            println("${other.creature.name} retaliates ${creature.name} for $damage damage: ${quantity} left (${creature.currentHealth} health)")
+        }
+    }
+
+    /**
+     * Return true if this creature stack is alive.
+     */
+    fun isAlive() = quantity > 0
+
     /**
      * Damage formula details are available in [Damage formula](https://heroes.thelazy.net/index.php/Damage).
      */
     fun computeDamage(other: Creature): Int {
         // Use the average creature damage instead of a random damage, to get a deterministic result
-        val baseDamage = (other.maxDamage - other.minDamage) * 0.5 * quantity;
+        val baseDamage = (creature.maxDamage + creature.minDamage) * 0.5 * quantity;
 
         // Compute the attack and defense bonuses
         val attackDifference = creature.attack - other.defense
@@ -31,11 +51,19 @@ class CreatureStack(val creature: Creature, val initialQuantity: Int) {
      * Apply some damage to this creature stack.
      */
     fun applyDamage(damage: Int) {
-        // The current total health of all creatures of the stack
-        val currentStackHealth = creature.initialHealth * (quantity - 1) + creature.currentHealth
-        val newStackHealth = max(currentStackHealth - damage, 0)
+        // A stack health is the total health of all creatures of the stack
+        val previousStackHealth = creature.initialHealth * (quantity - 1) + creature.currentHealth
+        val newStackHealth = previousStackHealth - damage
 
-        quantity = newStackHealth / creature.initialHealth
-        creature.currentHealth = newStackHealth % creature.initialHealth
+        if (newStackHealth <= 0) {
+            quantity = 0
+            creature.currentHealth = 0
+        } else if (newStackHealth % creature.initialHealth == 0) {
+            quantity = newStackHealth / creature.initialHealth
+            creature.currentHealth = creature.initialHealth
+        } else {
+            quantity = newStackHealth / creature.initialHealth + 1
+            creature.currentHealth = newStackHealth % creature.initialHealth
+        }
     }
 }
