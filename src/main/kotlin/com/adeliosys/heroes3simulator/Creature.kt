d763670762d80765b256498cc28a384ab3843774
Florian Beaufumé <fbeaufume@gmail.com>
2023-02-04 10:08:01 +0100

    Improved creatures construction

--- src/main/kotlin/com/adeliosys/heroes3simulator/Creature.kt
+++ src/main/kotlin/com/adeliosys/heroes3simulator/Creature.kt
@@ -3,17 +3,22 @@ package com.adeliosys.heroes3simulator
 /**
  * A creature descriptor.
  */
-class Creature(
+open class Creature(
     val name: String,
     val attack: Int,
     val defense: Int,
     val minDamage: Int,
     val maxDamage: Int,
-    val initialHealth: Int
+    val initialHealth: Int,
+    abilities: List<Ability> = listOf()
 ) {
     var currentHealth = initialHealth
 
     private val abilities: MutableList<Ability> = mutableListOf()
+
+    init {
+        this.abilities.addAll(abilities)
+    }
 
     /**
      * Reset the current health to the initial health.
@@ -35,3 +40,27 @@ class Creature(
      */
     fun hasAbility(ability: Ability): Boolean = abilities.contains(ability)
 }
+
+// See https://heroes.thelazy.net/index.php/List_of_creatures for creature stats
+
+class Peasant() : Creature("Peasant", 1, 1, 1, 1, 1)
+
+class SkeletonWarrior() : Creature("Skeleton Warrior", 6, 6, 1, 3, 6)
+
+class Halberdier() : Creature("Halberdier", 6, 5, 2, 3, 10)
+
+class BattleDwarf() : Creature("Battle Dwarf", 7, 7, 2, 4, 20)
+
+class SilverPegasus() : Creature("Silver Pegasus", 9, 10, 5, 9, 30)
+
+class Crusader() : Creature("Crusader", 12, 12, 7, 10, 35, listOf(Ability.DOUBLE_ATTACK))
+
+class MinotaurKing() : Creature("Minotaur King", 15, 15, 12, 20, 50)
+
+class NagaQueen() : Creature("Naga Queen", 16, 13, 30, 30, 110, listOf(Ability.NO_ENEMY_RETALIATION))
+
+class ArchDevil() : Creature("Arch Devil", 26, 28, 30, 40, 200, listOf(Ability.NO_ENEMY_RETALIATION))
+
+class Archangel() : Creature("Anchangel", 30, 30, 50, 50, 250)
+
+class AzureDragon() : Creature("Azure Dragon", 50, 50, 70, 80, 1000)
