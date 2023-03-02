06545ce0f40de36ddbf0c70436360378d2102152
Florian Beaufumé <fbeaufume@gmail.com>
2023-03-02 19:41:41 +0100

    Added creature speed

--- src/main/kotlin/com/adeliosys/heroes3simulator/Creature.kt
+++ src/main/kotlin/com/adeliosys/heroes3simulator/Creature.kt
@@ -10,6 +10,7 @@ open class Creature(
     val minDamage: Int,
     val maxDamage: Int,
     val initialHealth: Int,
+    val speed: Int,
     abilities: List<Ability> = listOf()
 ) {
     var currentHealth = initialHealth
@@ -43,24 +44,24 @@ open class Creature(
 
 // See https://heroes.thelazy.net/index.php/List_of_creatures for creature stats
 
-class Peasant() : Creature("Peasant", 1, 1, 1, 1, 1)
+class Peasant() : Creature("Peasant", 1, 1, 1, 1, 1, 3)
 
-class SkeletonWarrior() : Creature("Skeleton Warrior", 6, 6, 1, 3, 6)
+class SkeletonWarrior() : Creature("Skeleton Warrior", 6, 6, 1, 3, 6, 5)
 
-class Halberdier() : Creature("Halberdier", 6, 5, 2, 3, 10)
+class Halberdier() : Creature("Halberdier", 6, 5, 2, 3, 10, 5)
 
-class BattleDwarf() : Creature("Battle Dwarf", 7, 7, 2, 4, 20)
+class BattleDwarf() : Creature("Battle Dwarf", 7, 7, 2, 4, 20, 5)
 
-class SilverPegasus() : Creature("Silver Pegasus", 9, 10, 5, 9, 30)
+class SilverPegasus() : Creature("Silver Pegasus", 9, 10, 5, 9, 30, 12)
 
-class Crusader() : Creature("Crusader", 12, 12, 7, 10, 35, listOf(Ability.DOUBLE_ATTACK))
+class Crusader() : Creature("Crusader", 12, 12, 7, 10, 35,6, listOf(Ability.DOUBLE_ATTACK))
 
-class MinotaurKing() : Creature("Minotaur King", 15, 15, 12, 20, 50)
+class MinotaurKing() : Creature("Minotaur King", 15, 15, 12, 20, 50, 8)
 
-class NagaQueen() : Creature("Naga Queen", 16, 13, 30, 30, 110, listOf(Ability.NO_ENEMY_RETALIATION))
+class NagaQueen() : Creature("Naga Queen", 16, 13, 30, 30, 110, 7, listOf(Ability.NO_ENEMY_RETALIATION))
 
-class ArchDevil() : Creature("Arch Devil", 26, 28, 30, 40, 200, listOf(Ability.NO_ENEMY_RETALIATION))
+class ArchDevil() : Creature("Arch Devil", 26, 28, 30, 40, 200, 17, listOf(Ability.NO_ENEMY_RETALIATION))
 
-class Archangel() : Creature("Anchangel", 30, 30, 50, 50, 250)
+class Archangel() : Creature("Anchangel", 30, 30, 50, 50, 250, 18)
 
-class AzureDragon() : Creature("Azure Dragon", 50, 50, 70, 80, 1000)
+class AzureDragon() : Creature("Azure Dragon", 50, 50, 70, 80, 1000, 19)
