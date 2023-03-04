60551e7e57dd6b07ce923fc1563705e629a2f397
Florian Beaufumé <fbeaufume@gmail.com>
2023-03-04 12:24:27 +0100

    Added creature size

--- src/main/kotlin/com/adeliosys/heroes3simulator/Creature.kt
+++ src/main/kotlin/com/adeliosys/heroes3simulator/Creature.kt
@@ -11,6 +11,7 @@ open class Creature(
     val maxDamage: Int,
     val initialHealth: Int,
     val speed: Int,
+    val size: Int,
     val ranged: Boolean,
     private vararg val abilities: Ability
 ) {
@@ -31,30 +32,30 @@ open class Creature(
 
 // See https://heroes.thelazy.net/index.php/List_of_creatures for creature stats
 
-class Peasant() : Creature("Peasant", 1, 1, 1, 1, 1, 3, false)
+class Peasant() : Creature("Peasant", 1, 1, 1, 1, 1, 3, 1, false)
 
-class SkeletonWarrior() : Creature("Skeleton Warrior", 6, 6, 1, 3, 6, 5, false)
+class SkeletonWarrior() : Creature("Skeleton Warrior", 6, 6, 1, 3, 6, 5, 1, false)
 
-class Halberdier() : Creature("Halberdier", 6, 5, 2, 3, 10, 5, false)
+class Halberdier() : Creature("Halberdier", 6, 5, 2, 3, 10, 5, 1, false)
 
-class Marksman(): Creature("Marksman", 6, 3, 2, 3, 10, 6, true, Ability.DOUBLE_ATTACK)
+class Marksman() : Creature("Marksman", 6, 3, 2, 3, 10, 6, 1, true, Ability.DOUBLE_ATTACK)
 
-class BattleDwarf() : Creature("Battle Dwarf", 7, 7, 2, 4, 20, 5, false)
+class BattleDwarf() : Creature("Battle Dwarf", 7, 7, 2, 4, 20, 5, 1, false)
 
-class SilverPegasus() : Creature("Silver Pegasus", 9, 10, 5, 9, 30, 12, false)
+class SilverPegasus() : Creature("Silver Pegasus", 9, 10, 5, 9, 30, 12, 2, false)
 
-class Crusader() : Creature("Crusader", 12, 12, 7, 10, 35,6, false, Ability.DOUBLE_ATTACK)
+class Crusader() : Creature("Crusader", 12, 12, 7, 10, 35, 6, 1, false, Ability.DOUBLE_ATTACK)
 
-class ArchMage(): Creature("Arch Mage", 12, 9, 7, 9, 30, 7, true)
+class ArchMage() : Creature("Arch Mage", 12, 9, 7, 9, 30, 7, 1, true)
 
-class Zealot(): Creature("Zealot", 12, 10, 10, 12, 30, 7, true)
+class Zealot() : Creature("Zealot", 12, 10, 10, 12, 30, 7, 1, true)
 
-class MinotaurKing() : Creature("Minotaur King", 15, 15, 12, 20, 50, 8, false)
+class MinotaurKing() : Creature("Minotaur King", 15, 15, 12, 20, 50, 8, 1, false)
 
-class NagaQueen() : Creature("Naga Queen", 16, 13, 30, 30, 110, 7, false, Ability.NO_ENEMY_RETALIATION)
+class NagaQueen() : Creature("Naga Queen", 16, 13, 30, 30, 110, 7, 2, false, Ability.NO_ENEMY_RETALIATION)
 
-class ArchDevil() : Creature("Arch Devil", 26, 28, 30, 40, 200, 17, false, Ability.NO_ENEMY_RETALIATION)
+class ArchDevil() : Creature("Arch Devil", 26, 28, 30, 40, 200, 17, 1, false, Ability.NO_ENEMY_RETALIATION)
 
-class Archangel() : Creature("Anchangel", 30, 30, 50, 50, 250, 18, false)
+class Archangel() : Creature("Anchangel", 30, 30, 50, 50, 250, 18, 2, false)
 
-class AzureDragon() : Creature("Azure Dragon", 50, 50, 70, 80, 1000, 19, false)
+class AzureDragon() : Creature("Azure Dragon", 50, 50, 70, 80, 1000, 19, 2, false)
